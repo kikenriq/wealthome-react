f7f879cf8f8863236eb40bfc73c79e0d87c93c6e
kikenriq <108308253+kikenriq@users.noreply.github.com>
2023-10-05 13:09:12 -0400

    Hero Section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,12 +18,12 @@ const Header = () => {
             <img src="/images/logo.png" alt="" />
         </div>
 
-        <button className='bg-white cursor-pointer inline-flex rounded-xl text-[15px] p-2'
+        <button className='bg-white cursor-pointer inline-flex -rounded--radius-small text-[15px] p-2'
         onClick={() => setMobileNav(!mobileNav)}>
           <span class="material-symbols-outlined">menu</span>
         </button>
 
-        <div className={`${mobileNav ? ' opacity-100 scale-100' : ' opacity-0 scale-50'} transition-all origin-top-right absolute text-[15px] top-[75%] z-40 right-[16px] shadow-md bg-neutral-100 min-w-[260px] px-8 py-8 rounded-3xl`}>
+        <div className={`${mobileNav ? ' opacity-100 scale-100' : ' opacity-0 scale-50'} transition-all origin-top-right absolute text-[15px] top-[75%] z-40 right-[16px] shadow-md bg-neutral-100 min-w-[260px] px-8 py-8 -rounded--radius-large`}>
           <ul className='grid gap-4'>
             <li><a className='px-1 py-3 text-neutral-30 hover:text-primary-80 font-semibold' href="#">Home</a></li>
             <li><a className='px-1 py-3 text-neutral-30 hover:text-primary-80 font-semibold' href="#">Rent</a></li>
@@ -32,7 +32,7 @@ const Header = () => {
             <li><a className='px-1 py-3 text-neutral-30 hover:text-primary-80 font-semibold' href="#">About</a></li>
             <li><a className='px-1 py-3 text-neutral-30 hover:text-primary-80 font-semibold' href="#">Login</a></li>
           </ul>
-          <button className='px-8 py-5 bg-primary-100 text-neutral-100 rounded-3xl'>
+          <button className='px-8 py-5 bg-primary-100 text-neutral-100 -rounded--radius-medium'>
             <a href="#">Get Started</a>
           </button>
         </div>
